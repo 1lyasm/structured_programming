9decb5b1425f710079479ddde6e3559cb26c6780
1lyasm <1lyasm.dev@gmail.com>
2024-03-13 15:51:50 +0300

    Add formatting examples to format_strings

--- format_strings/main.c
+++ format_strings/main.c
@@ -2,10 +2,16 @@
 
 int main(void) {
     int number = 65;
+    float floatNumber = 700.555;
 
     printf("Number as long int: %li\n", number);
     printf("Number as unsigned int: %u\n", number);
     printf("Number as character: %c\n", number);
+    printf("010 as octal: %o\n", 010);
+    printf("010 as decimal: %d\n", 010);
+    printf("123 in scientific notation: %e\n", 123.);
+    printf("Float number as double: %.2f\n", floatNumber);
+    printf("Float number in exponential form: %e\n", floatNumber);
 
     return 0;
 }
