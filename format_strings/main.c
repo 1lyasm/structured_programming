585a1ef48daf1f095d68befdd58b88e3b27d603f
1lyasm <1lyasm.dev@gmail.com>
2024-03-13 15:43:03 +0300

    Add format_strings

---
+++ format_strings/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void) {
+    int number = 65;
+
+    printf("Number as long int: %li\n", number);
+    printf("Number as unsigned int: %u\n", number);
+    printf("Number as character: %c\n", number);
+
+    return 0;
+}
