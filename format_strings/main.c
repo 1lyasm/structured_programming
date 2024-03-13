ab5f69b7b224fa9715e6ea88dd05861664de4fea
1lyasm <1lyasm.dev@gmail.com>
2024-03-13 16:07:54 +0300

    Add subtraction between signed and unsigned integers example to format_strings

--- format_strings/main.c
+++ format_strings/main.c
@@ -3,6 +3,8 @@
 int main(void) {
     int number = 65;
     float floatNumber = 700.555;
+    int subtractionResult = 10u - 15;
+    unsigned int unsignedSubtractionResult = 10u - 15;
 
     printf("Number as long int: %li\n", number);
     printf("Number as unsigned int: %u\n", number);
@@ -12,6 +14,9 @@ int main(void) {
     printf("123 in scientific notation: %e\n", 123.);
     printf("Float number as double: %.2f\n", floatNumber);
     printf("Float number in exponential form: %e\n", floatNumber);
+
+    printf("Subtraction result: %d\n", subtractionResult);
+    printf("Unsigned subtraction result: %u\n", unsignedSubtractionResult);
 
     return 0;
 }
