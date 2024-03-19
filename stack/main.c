ac2a70e0eeb52bbb4bd7819f5d15bdaa202a438d
1lyasm <1lyasm.dev@gmail.com>
2024-03-19 23:31:25 +0300

    Add Stack struct

--- stack/main.c
+++ stack/main.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 
+#include "stack.h"
+
 int main(void) {
+    Stack *stack = NULL;
+
+    printf("%p\n", (void*) stack);
+
     return 0;
 }
 
---
+++ stack/stack.h
@@ -0,0 +1,10 @@
+#ifndef STACK_H
+#define STACK_H
+
+typedef struct {
+    int _start_index;
+    int _end_index;
+    void *_buffer;
+} Stack;
+
+#endif
