3d5d2fbbb892666b6e2b9a9931812612633f5619
1lyasm <1lyasm.dev@gmail.com>
2024-03-20 00:00:46 +0300

    Add failMalloc

--- stack/stack.c
+++ stack/stack.c
@@ -1,6 +1,22 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 #include "stack.h"
+
+/*
+ * failMalloc is a wrapper for malloc that fails (exits process) if malloc call
+ * returns error code
+ */
+static void *failMalloc(size_t nBytes) {
+    void *buffer;
+
+    buffer = malloc(nBytes);
+    if (buffer == NULL) {
+        fprintf(stderr, "failMalloc: malloc failed\n");
+    }
+
+    return buffer;
+}
 
 Stack *constructStack(void) {
     printf("constructStack is called\n");
